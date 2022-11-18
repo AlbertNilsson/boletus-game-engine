61d01c6f02ca61c9f73116782a39fe20124e1311
Albert Nilsson <albertnilsson1998@gmail.com>
2022-11-18 06:57:34 +0000

    Change color of triangle at runtime

--- test-graphics/window.c
+++ test-graphics/window.c
@@ -3,6 +3,7 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>  
 #include <stdlib.h>  
+#include <math.h>
 #include "files.h"
 
 
@@ -10,6 +11,9 @@ int main() {
  char *const vertexSrcTmp = malloc(1000);
  char *const fragmentSrcTmp = malloc(1000);
  char buffer[512];
+ GLuint vao, vbo, shaderProgram, posAttrib, vertexShader;
+ GLuint fragmentShader ;
+ GLint status, uniColor;
  float vertices[] = {
     0.0f,  0.5f, // Vertex 1 (X, Y)
     0.5f, -0.5f, // Vertex 2 (X, Y)
@@ -28,6 +32,7 @@ int main() {
   const char *const vertexSrc = vertexSrcTmp;
   const char *const fragmentSrc = fragmentSrcTmp;
 
+
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
@@ -40,17 +45,17 @@ int main() {
 
   glewExperimental = GL_TRUE;
   glewInit();
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
 
-  GLuint vbo;
   glGenBuffers(1, &vbo);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vertexShader, 1, &vertexSrc, NULL);
   glCompileShader(vertexShader);
 
-  GLint status = GL_FALSE;
   glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
   if(status == GL_TRUE) {
     fprintf(fptr, "%s\n", "Vertex shader succesfully compiled");
@@ -58,7 +63,7 @@ int main() {
     fprintf(fptr, "%s\n", "Vertex shader failed to compiled");
   }
 
-  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fragmentShader, 1, &fragmentSrc, NULL);
   glCompileShader(fragmentShader);
 
@@ -70,7 +75,7 @@ int main() {
     fprintf(fptr, "%s\n", "Fragment shader failed to compiled");
   }
 
-  GLuint shaderProgram = glCreateProgram();
+  shaderProgram = glCreateProgram();
   glAttachShader(shaderProgram, vertexShader);
   glAttachShader(shaderProgram, fragmentShader);
   //fprintf(fptr, "%s %d\n", "Errors1: ", glGetError());
@@ -82,20 +87,23 @@ int main() {
     fprintf(fptr, "%s\n", "Vertex shader succesfully linked");
   else if(status == GL_FALSE)
     fprintf(fptr, "%s\n", "Vertex shader failed to be  linked");
+
   glUseProgram(shaderProgram);
-  GLuint vao;
-  glGenVertexArrays(1, &vao);
-  glBindVertexArray(vao);
-  GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+  posAttrib = glGetAttribLocation(shaderProgram, "position");
   glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
   glEnableVertexAttribArray(posAttrib);
+
+  uniColor = glGetUniformLocation(shaderProgram, "triangleColor");
+  glUniform3f(uniColor, 0.0f, 1.0f, 0.0f);
+  fprintf(fptr, "%s %d\n", "Error: ", glGetError());
   fprintf(fptr, "%s %s\n", "GL version: ",  glGetString(GL_VERSION));
 
-
-
-
-  while(!glfwWindowShouldClose(window))
-  {
+  float count = 0.01;
+  float color = 0; 
+  while(!glfwWindowShouldClose(window)) {
+    color = sin(count*2.0f)/2.0f;
+    glUniform3f(uniColor, 1-color, color, 0.0f);
+    count += 0.01;
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
       glfwSetWindowShouldClose(window, GL_TRUE);
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
